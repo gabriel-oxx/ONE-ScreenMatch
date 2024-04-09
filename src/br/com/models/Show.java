3aeadebedfa10328f28c9d8a0631851cddc88e12
Gabriel <gabrielcsilvasg@gmail.com>
2024-04-09 08:15:34 -0300

    Adicionar classe Show

---
+++ src/br/com/models/Show.java
@@ -0,0 +1,53 @@
+package br.com.models;
+
+public class Show extends Title {
+	private int seasons;
+	private int episodesBySeason;
+	private boolean isActive;
+	private int minutesByEpisode;
+
+	public void setSeasons(int seasons) {
+		this.seasons = seasons;
+	}
+
+	public void setEpisodesBySeason(int episodesBySeason) {
+		this.episodesBySeason = episodesBySeason;
+	}
+
+	public void setIsActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public void setMinutesByEpisode(int minutesByEpisode) {
+		this.minutesByEpisode = minutesByEpisode;
+	}
+
+	public int getSeasons() {
+		return seasons;
+	}
+
+	public int getEpisodesBySeason() {
+		return episodesBySeason;
+	}
+
+	public boolean getIsActive() {
+		return isActive;
+	}
+
+	public int getMinutesByEpisode() {
+		return minutesByEpisode;
+	}
+
+	@Override
+	public int getDurationInMinutes() {
+		return getSeasons() * getEpisodesBySeason() * getMinutesByEpisode();
+	}
+
+	@Override
+	public void displayDetails() {
+		System.out.println("Nome da serie: " + getName());
+		System.out.println("Ano de lançamento: " + getReleaseYear());
+		System.out.println("Temporadas: " + getSeasons());
+		System.out.println("Episódios por temporada: " + getEpisodesBySeason());
+	}
+}
