1243001bc8bc7f12a1bf5391552ad3418b135b4a
Gabriel <gabrielcsilvasg@gmail.com>
2024-04-10 14:41:56 -0300

    Corrigir bugs

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		Movie movie = new Movie();
 		movie.setName("Star Wars");
-		movie.evaluate(4);
-		movie.evaluate(6);
-		movie.evaluate(9);
-		movie.evaluate(4);
 		Show lost = new Show();
 		lost.setName("Lost");
 		lost.setSeasons(4);
@@ -19,9 +15,9 @@ public class Main {
 		Episode episode = new Episode();
 		episode.setNumber(1);
 		episode.setShow(lost);
-		episode.setTotalViews(800);
+		episode.setTotalViews(45);
 		RecommendationFilter filter = new RecommendationFilter();
-		filter.filter(lost);
+		filter.filter(episode);
 	}
 }
 
